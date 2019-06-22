c6e04b1b74d7dadf4b35d702b3679f5a89512dfc
Szabó Marcell <marcelion1999@gmail.com>
2019-06-22 12:31:20 +0200

    ver 0.1.1

--- index.php
+++ index.php
@@ -5,14 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <style>
+    .jumbotron
+    {
+      background-image: url(images/bg.png); background-size: 100% no-repeat center center fixed;
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      -o-background-size: cover;
+      background-size: cover;
+      color:white;
+      height:400px;
+    }
+    </style>
     <title>fr-demo</title>
   </head>
   <body>
     <?php include("navbar.html") ?>
-    
 
-
+<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+  <div class="col-md-5 p-lg-5 mx-auto my-5">
+    <h1 class="display-4 font-weight-normal">Punny headline</h1>
+    <p class="lead font-weight-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta temporibus adipisci ea facilis optio aliquid quidem dolorum repudiandae sapiente aperiam autem quos voluptatem eaque, saepe cumque harum suscipit asperiores quia?</p>
+  <a class="btn btn-outline-secondary" href="#">Coming soon</a>
+  </div>
+  <div class="product-device shadow-sm d-none d-md-block"></div>
+  <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+</div>
 
 
 
