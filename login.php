dd4514b9711053caa396230ea3e01916d9cf7845
Szabó Marcell <marcelion1999@gmail.com>
2019-07-06 13:59:40 +0200

    Hibajavítások, már nem bugos

--- login.php
+++ login.php
@@ -3,15 +3,16 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    
-    <link rel="stylesheet" href="style.css">
 
-    <link href="login.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="src\style.css">
+    
+    <link rel="stylesheet" href="src\login.css">
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" 
     integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
@@ -27,14 +28,23 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
     <link href="https://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
-   
+    
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+
+    <script src="src/jquery.MultiLanguage.min.js"></script>
+
+    <script>
+                $(document).ready(function()
+                    {
+                    $.MultiLanguage('language.json');
+                    }
+                )
+    </script>
+
     <title>Capacity Sharing</title>
+    <link rel="shortcut icon" href="src/images/ico/favicon.ico">
 
-    <link rel="shortcut icon" href="images/ico/favicon.ico">
-	
-
-	
-</head>
+  </head>
 <body>
 
 
--- prices.php
+++ prices.php
@@ -3,45 +3,44 @@
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+
+    <link rel="stylesheet" href="src\style.css">
+
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" 
     integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
    
     <!-- Scripts-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
     <link href="https://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
-   
+    
     <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
 
+    <script src="src/jquery.MultiLanguage.min.js"></script>
 
-<script src="jquery.MultiLanguage.min.js"></script>
+    <script>
+                $(document).ready(function()
+                    {
+                    $.MultiLanguage('language.json');
+                    }
+                )
+    </script>
 
-<script>
-$(document).ready(function(){
-$.MultiLanguage('language.json');
-})
-</script>
-<script src="src/jquery.MultiLanguage.min.js"></script>
-
-<script>
-        $(document).ready(function()
-        {
-          $.MultiLanguage('language.json');
-        }
-        )
-</script>
- 
     <title>Capacity Sharing</title>
-    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="shortcut icon" href="src/images/ico/favicon.ico">
 
   </head>
   <body>
@@ -53,17 +52,19 @@ $.MultiLanguage('language.json');
         Túrbózza fel vállalkozását egyedi üzleti oldalunkal
     </div>
 
-                <?php 
-                    include("navbar.html");
-                ?>
-
+                    <?php 
+                        include("src/navbar.html");
+                        //<!-- Script Cookie-->
+                        include("src/cookie.html");
+                        //<!-- Script Cookie-->
+                    ?>
 
             <div class="container">
                 <div class="jumbotron">
 
                     <div class="box">
                     <div class="center gap">
-                            <h2 class="center">Példa Ismertető</h2><hr>
+                            <h2 class="center" id="demo"></h2><hr>
                             <p class="lead"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt nemo reprehenderit magni.
                                  Sapiente similique assumenda a, praesentium rerum nulla tenetur animi, earum esse culpa explicabo corporis sed eum, commodi qui? 
                                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae eveniet magni eum commodi quis unde, ea id ipsa nemo tempore 
@@ -128,7 +129,8 @@ $.MultiLanguage('language.json');
  
 
 
-    <?php include("footer.html"); ?>
+
+            <?php include("src/footer.html"); ?>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
